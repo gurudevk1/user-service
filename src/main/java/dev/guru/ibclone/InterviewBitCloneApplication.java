80daf36ca672ed345c34a3d1416665f462934e29
Guru <gurudevkumar11@gmail.com>
2020-10-25 23:25:33 +0530

    refactor

--- src/main/java/dev/guru/ibclone/InterviewBitCloneApplication.java
+++ src/main/java/dev/guru/ibclone/InterviewBitCloneApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class UserServiceApplication {
+public class InterviewBitCloneApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(InterviewBitCloneApplication.class, args);
     }
 
 }
